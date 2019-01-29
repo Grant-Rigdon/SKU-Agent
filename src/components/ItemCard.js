8a44fca64730d7e654ba1b28698e541420903bde
Grant Rigdon <grant_rigdon@ymail.com>
2019-01-28 20:33:24 -0500

    components added

---
+++ src/components/ItemCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Badge from '@material-ui/core/Badge'
+
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+});
+
+function Item(props) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <Paper className={classes.root} elevation={1}>
+        <Typography variant="h5" component="h3">
+          This is a sheet of paper.
+        </Typography>
+        <Typography component="p">
+          Paper can be used to build surface or other elements for your application.
+        </Typography>
+        <Badge color="primary" badgeContent={4} className={classes.margin}>
+            <Button variant="contained">Button</Button>
+        </Badge>
+      </Paper>
+    </div>
+  );
+}
+
+Item.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Item);
---
+++ src/components/Login.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import classNames from 'classnames'
+import IconButton from '@material-ui/core/IconButton'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import TextField from '@material-ui/core/TextField'
+import Visibility from '@material-ui/icons/Visibility'
+import VisibilityOff from '@material-ui/icons/VisibilityOff'
+import Button from '@material-ui/core/Button'
+
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+})
+
+class Login extends React.Component {
+    state = {      
+      password: '',      
+      showPassword: false,
+    }
+  
+    handleChange = prop => event => {
+      this.setState({ [prop]: event.target.value })
+    }
+  
+    handleClickShowPassword = () => {
+      this.setState(state => ({ showPassword: !state.showPassword }))
+    }
+  
+    render() {
+        const {classes} = this.props
+        return (
+            <div>
+            <Paper className={classes.root} elevation={1}>
+                <TextField
+                    id="outlined-name"
+                    label="Username"
+                    className={classes.textField}
+                    value={this.state.name}
+                    onChange={this.handleChange('name')}
+                    margin="normal"
+                    variant="outlined"
+                />
+                <br />
+                <TextField
+                    id="outlined-adornment-password"
+                    className={classNames(classes.margin, classes.textField)}
+                    variant="outlined"
+                    type={this.state.showPassword ? 'text' : 'password'}
+                    label="Password"
+                    value={this.state.password}
+                    onChange={this.handleChange('password')}
+                    InputProps={{
+                        endAdornment: (
+                        <InputAdornment position="end">
+                            <IconButton
+                            aria-label="Toggle password visibility"
+                            onClick={this.handleClickShowPassword}
+                            >
+                            {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                        ),
+                    }}
+                />
+                <br />
+                <Button variant="contained" color="primary" className={classes.button}>
+                    Login
+                </Button>
+                
+            </Paper>
+            </div>
+        )
+    }
+}
+
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Login)
