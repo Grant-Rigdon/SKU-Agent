dc1f1eb582ebd4a20fcdfd775b0e5688903d7e25
David Hiller <HillerDavid@users.noreply.github.com>
2019-02-02 10:00:19 -0500

    commit before pulldown

--- client/src/components/Accordion.js
+++ client/src/components/Accordion.js
@@ -7,6 +7,7 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import StorageTable from './Table'
+import Button from '@material-ui/core/Button'
 
 const styles = theme => ({
   root: {
@@ -58,6 +59,9 @@ class Accordion extends React.Component {
           <ExpansionPanelDetails >
             <StorageTable />
           </ExpansionPanelDetails>
+          <Button onClick={this.handleClose} color="primary">
+              Ok
+            </Button>
         </ExpansionPanel>
       </div>
     )
