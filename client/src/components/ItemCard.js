2d7257dec02b44e93db33692e954e8e8df18cc3c
David Hiller <HillerDavid@users.noreply.github.com>
2019-02-11 20:15:57 -0500

    Adjusted changes in ItemCard for push

--- client/src/components/ItemCard.js
+++ client/src/components/ItemCard.js
@@ -1,43 +1,44 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
-import Chip from '@material-ui/core/Chip';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
 
 
 
 const styles = theme => ({
   root: {
-    display: 'flex',
-    justifyContent: 'center',
-    flexWrap: 'wrap',
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    paddingLeft: theme.spacing.unit * 2,
+    minWidth: 160,
+    maxWidth: 320,
+    height: 'auto'
   },
-  chip: {
-    margin: theme.spacing.unit,
+  buttonContainer: {
+    marginTop: theme.spacing.unit * 2
   },
-});
-
-
-class Item extends Component {
-
-
-  handleClick = () => {
-    console.log('clicked')
+  button: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  badge: {
+    backgroundColor: theme.palette.secondary.main
   }
+})
 
-  render() {
-    const { classes } = this.props
+
+  function Item(props) {
+    const { classes } = props
+  
     return (
-      <div className={classes.root}>
-        <Chip
-          avatar={<Avatar>MB</Avatar>}
-          label={this.props.name}
-          onClick={this.handleClick}
-          className={classes.chip}
-        />
+      <div>
+        <Paper className={classes.root} elevation={1}>
+          <Typography variant="h5" component="h3">
+            {props.name}
+          </Typography>
+        </Paper>
       </div>
     )
-  }
 
 }
 
